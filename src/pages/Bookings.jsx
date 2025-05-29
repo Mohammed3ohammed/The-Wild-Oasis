9172394ee60f18cee2eeb209505a7049dce6328c
Mohammed Mohammed <142446903+Mohammed3ohammed@users.noreply.github.com>
2025-05-29 23:08:43 +0300

    feat: create AppLayout component with Sidebar and Header

---
+++ src/pages/Bookings.jsx
@@ -0,0 +1,11 @@
+
+
+function Bookings() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Bookings
---
+++ src/pages/Cabins.jsx
@@ -0,0 +1,11 @@
+
+
+function Cabins() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Cabins
