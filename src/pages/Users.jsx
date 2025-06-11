c8f5fd63365a996d8e074cd1dde6fbaa4151aadf
Mohammed Mohammed <142446903+Mohammed3ohammed@users.noreply.github.com>
2025-06-11 18:10:35 +0300

    feat: add Users page with create user heading

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,11 +1,7 @@
-
+import Heading from "../ui/Heading";
 
 function Users() {
-  return (
-    <div>
-      
-    </div>
-  )
+  return <Heading as="h1">Create a new user</Heading>
 }
 
-export default Users
+export default Users;
