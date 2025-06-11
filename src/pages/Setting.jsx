a73a76b816a18b028bff25588f45de912dfded18
Mohammed Mohammed <142446903+Mohammed3ohammed@users.noreply.github.com>
2025-06-11 18:06:37 +0300

    feat: add Setting page with hotel settings heading

--- src/pages/Setting.jsx
+++ src/pages/Setting.jsx
@@ -1,11 +1,7 @@
-
+import Heading from "../ui/Heading";
 
 function Setting() {
-  return (
-    <div>
-      
-    </div>
-  )
+  return <Heading as="h1">Update hotel settings</Heading>
 }
 
-export default Setting
+export default Setting;
