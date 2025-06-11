651e178d18a04671d2253da47f3d5432cdbaf2db
Mohammed Mohammed <142446903+Mohammed3ohammed@users.noreply.github.com>
2025-06-11 17:49:59 +0300

    feat: add Bookings page with Heading and test paragraph

--- src/pages/Bookings.jsx
+++ src/pages/Bookings.jsx
@@ -1,11 +1,13 @@
-
+import Heading from "../ui/Heading";
+import Row from "../ui/Row";
 
 function Bookings() {
   return (
-    <div>
-      
-    </div>
+    <Row type="horizontal">
+      <Heading as="h1">All bookings</Heading>
+      <p>TEST</p>
+    </Row>
   )
 }
 
-export default Bookings
+export default Bookings;
