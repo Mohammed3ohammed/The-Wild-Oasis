a1c61792caf76923d828c141660d97b8005baa37
Mohammed Mohammed <142446903+Mohammed3ohammed@users.noreply.github.com>
2025-06-11 22:52:01 +0300

    feat(settings): add get and update functions for settings table

---
+++ src/services/apiSettings.js
@@ -0,0 +1,23 @@
+export async function getSettings() {
+    const {data, error} = await supabase.from("settings").select("*").single();
+
+    if (error) {
+        console.error(error);
+        throw new Error("Settings could not bs loaded");
+    }
+    return data;
+}
+
+export async function updateSetting(newSetting) {
+    const {data, error} = await supabase
+    .from("settings")
+    .update(newSetting)
+    .eq("id", 1)
+    .single();
+
+    if (error) {
+        console.error(error);
+        throw new Error("Settings could not be update");
+    }
+    return data;
+}
