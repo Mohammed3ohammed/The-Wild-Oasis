9bf81e13d5e0c58d2f7a2b66cb5c3eeba4259a4a
Mohammed Mohammed <142446903+Mohammed3ohammed@users.noreply.github.com>
2025-06-15 21:16:02 +0300

    Implement BookingTable with dynamic rows, loading state, and pagination

---
+++ src/features/check-in-out/BookingTable.jsx
@@ -0,0 +1,44 @@
+import BookingRow from '../../features/bookings/BookingRow';
+import Spinner from '../../ui/Spinner';
+import Table from '../../ui/Table';
+import { useBookings } from 'features/bookings/useBookings';
+import Menus from '../../ui/Menus';
+import Pagination from '../../ui/Pagination';
+import Empty from '../../ui/Empty';
+
+
+function BookingTable() {
+    const { bookings, count, isLoading } = useBookings();
+
+    if (isLoading) return <Spinner />;
+    if (!bookings) return <Empty resour={'bookings'} />;
+
+    return (
+        <Menus>
+            <Table columns='0.6fr 2fr 2.4fr 1.4fr 1fr 3.2rem'>
+                    <Table.Header>
+                        <div>Cabin</div>
+                        <div>Guest</div>
+                        <div>Dates</div>
+                        <div>Status</div>
+                        <div>Amount</div>
+                        <div></div>
+                    </Table.Header>
+
+                    <Table.Body
+                    data={bookings}
+                    render={(booking) => (
+                        <BookingRow key={booking.id} booking={booking} />
+                    )}
+                    />
+
+                    <Table.Footer>
+                        <Pagination count={count} />
+                    </Table.Footer>
+
+            </Table>
+        </Menus>
+    )
+}
+
+export default BookingTable;
