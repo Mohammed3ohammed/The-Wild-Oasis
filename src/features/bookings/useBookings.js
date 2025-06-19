3ce5da2d44ef2f4b76df1d5cb67290527986f0d0
Mohammed Mohammed <142446903+Mohammed3ohammed@users.noreply.github.com>
2025-06-19 19:58:17 +0300

    feat: add delete booking hook with mutation and toast notifications

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -14,8 +14,8 @@ export function useBookings() {
     const sortByRow = searchParams.get("sortBy") || "startDate-desc";
     const [field, direction] = sortByRow.split("-");
     const sortBy = {field, direction};
-
-    const page = !searchParams.get("page") ? 1 : Number(searchParams.get(page));
+    
+  const page = !searchParams.get("page") ? 1 : Number(searchParams.get("page"));
 
     const { isLoading, data: {data: bookings, count} = {}, error, } = useQuery({
         queryKey: ["bookings", filter, sortBy, page],
@@ -33,7 +33,7 @@ export function useBookings() {
     if (page > 1) 
         queryClient.prefetchQuery({
             queryKey: ["bookings", filter, sortBy, page - 1],
-            queryFn: () => getBookings({filter, sortBy, Page: page - 1}),
+            queryFn: () => getBookings({filter, sortBy, page: page - 1}),
         })
 
         return {isLoading, error, bookings, count};
