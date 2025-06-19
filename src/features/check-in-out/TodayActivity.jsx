5cacc53a7ea618168d960cc58ac3d1015880f59a
Mohammed Mohammed <142446903+Mohammed3ohammed@users.noreply.github.com>
2025-06-19 23:08:47 +0300

    feat: implement TodayActivity component to display today's activities with loading and empty states

--- src/features/check-in-out/TodayActivity.jsx
+++ src/features/check-in-out/TodayActivity.jsx
@@ -1,13 +1,18 @@
-import styled from 'styled-components';
-import { useActivityTodayStays } from './useActivityTodayStays';
-import { box } from 'styles/styles';
-import Heading from '../../ui/Heading';
-import Row from '../../ui/Row';
-import Spinner from '../../ui/Spinner';
-import TodayItem from '../dashboard/TodayItem';
+import styled from "styled-components";
+
+import Heading from "../../ui/Heading";
+import Row from "../../ui/Row";
+
+import { useTodayActivity } from "./useTodayActivity";
+import Spinner from "../../ui/Spinner";
+import TodayItem from "./TodayItem";
 
 const StyledToday = styled.div`
-  ${box}
+  /* Box */
+  background-color: var(--color-grey-0);
+  border: 1px solid var(--color-grey-100);
+  border-radius: var(--border-radius-md);
+
   padding: 3.2rem;
   display: flex;
   flex-direction: column;
@@ -35,71 +40,30 @@ const NoActivity = styled.p`
   margin-top: 0.8rem;
 `;
 
+function TodayActivity() {
+    const { activities, isLoading } = useTodayActivity();
+    
+  return (
+    <StyledToday>
+      <Row type="horizontal"> 
+        <Heading as="h2">Today</Heading>
+      </Row>
 
-function Today() {
-       const {isLoading, stays} = useActivityTodayStays();
-    return(
-        <StyledToday>
-            <Row type='horizontal'>
-                <Heading type='h2'>Today</Heading>
-            </Row>
-            {
-                !isLoading ? (
-                    stays.length > 0 ? (
-                        <TodayList>
-                            {stays.map((stay) => (
-                                <TodayItem key={stay.id} stay={stay} />
-                            ))}
-                            
-                        </TodayList>
-                        ) : (
-                            <NoActivity>No activity today...</NoActivity>
-                        )
-                    ) : (
-                    <Spinner />
-                )}
-        </StyledToday>
-    );
+       {!isLoading ? (
+        activities?.length > 0 ? (
+          <TodayList>
+            {activities.map((activity) => (
+              <TodayItem activity={activity} key={activity.id} />
+            ))}
+          </TodayList>
+        ) : (
+          <NoActivity>No activity today...</NoActivity>
+        )
+      ) : (
+        <Spinner />
+      )}
+    </StyledToday>
+  )
 }
 
-export default Today;
-
-
-const OLDdata = [
-  {
-    id: 1,
-    status: 'unconfirmed',
-    guests: { fullName: 'Jonas Schmedtmann' },
-    numNights: 6,
-  },
-  {
-    id: 2,
-    status: 'unconfirmed',
-    guests: { fullName: 'Steven Miller' },
-    numNights: 1,
-  },
-  {
-    id: 3,
-    status: 'checked-in',
-    guests: { fullName: 'John Smith' },
-    numNights: 3,
-  },
-  {
-    id: 4,
-    status: 'unconfirmed',
-    guests: { fullName: 'Marta Schmedtmann' },
-    numNights: 14,
-  },
-  {
-    id: 5,
-    status: 'checked-in',
-    guests: { fullName: 'Miguel Silva' },
-    numNights: 5,
-  },
-  {
-    id: 6,
-    status: 'checked-in',
-    guests: { fullName: 'Mary Williams' },
-    numNights: 4,
-  },
-];
+export default TodayActivity;
