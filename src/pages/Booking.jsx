ff2f923947ef4bb720f49e926e3e9a4f60c14dd1
Mohammed Mohammed <142446903+Mohammed3ohammed@users.noreply.github.com>
2025-06-20 23:39:44 +0300

    Create Bookings page with heading, table, and operations panel

---
+++ src/pages/Booking.jsx
@@ -0,0 +1,8 @@
+import BookingDetail from "../features/bookings/BookingDetail";
+
+function Bookings() {
+  
+       return <BookingDetail />;
+}
+
+export default Bookings;
--- src/pages/Bookings.jsx
+++ src/pages/Bookings.jsx
@@ -1,8 +1,21 @@
-import BookingDetail from "../features/bookings/BookingDetail";
+import Heading from "../ui/Heading";
+import Row from "../ui/Row";
+import BookingTable from "../features/bookings/BookingTable";
+import BookingTableOperations from "../features/bookings/BookingTableOperations";
+
 
 function Bookings() {
-  
-       return <BookingDetail />;
-}
+    return (
+        <>
+            <Row type="horizontal">
+                <Heading as="h1">All bookings</Heading>
+                <BookingTableOperations />
+            </Row>
+
+            <BookingTable />
+
+        </>
+    );
+}   
 
 export default Bookings;
