4ebb7ba9f6ce9161a645db1744fa87d5310efca0
Mohammed Mohammed <142446903+Mohammed3ohammed@users.noreply.github.com>
2025-06-20 23:45:49 +0300

    Create Cabins page with table, filters, and add form

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,12 +1,24 @@
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import CabinTableOperations from "../features/cabins/CabinTableOperations";
+import CabinTable from "../features/cabins/CabinTable";
+import AddCabin from "../features/cabins/AddCabin";
+
 
 function Cabins() {
   return (
+<>
     <Row type="horizontal">
         <Heading as="h1">Al cabins</Heading>
-        <p>TEST</p>
+        <CabinTableOperations />
     </Row>
+
+          <Row>
+        <CabinTable />
+        <AddCabin />
+      </Row>
+</>
+
   )
 }
 
