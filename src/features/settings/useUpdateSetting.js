e4c6ba54b40816ab0e791e09c70aea2ad5e7d096
Mohammed Mohammed <142446903+Mohammed3ohammed@users.noreply.github.com>
2025-11-16 21:38:19 +0200

    Add UserAvatar and HeaderMenu components

--- src/features/settings/useUpdateSetting.js
+++ src/features/settings/useUpdateSetting.js
@@ -3,16 +3,16 @@ import { toast } from "react-hot-toast";
 import { updateSetting as updateSettingApi } from "../../services/apiSettings";
 
 export function useUpdateSetting() {
-    const queryClient = useQueryClient();
+  const queryClient = useQueryClient();
 
-    const { mutate: updateSetting, isLoading: isUpdating } = useMutation({
-        mutationFn: updateSettingApi,
-        onSuccess: () => {
-            toast.success("Setting successfully edited");
-            queryClient.invalidateQueries({ queryKey: ["settings"]});
-        },
-        onError: (err) => toast.error(err.message),
-    });
+  const { mutate: updateSetting, isLoading: isUpdating } = useMutation({
+    mutationFn: updateSettingApi,
+    onSuccess: () => {
+      toast.success("Setting successfully edited");
+      queryClient.invalidateQueries({ queryKey: ["settings"] });
+    },
+    onError: (err) => toast.error(err.message),
+  });
 
-    return {isUpdating, updateSetting};
+  return { isUpdating, updateSetting };
 }
